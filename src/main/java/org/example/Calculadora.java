9bcc7ba0a98fd6577fe6ea2dddb3554e1630854f
Bruno Mantovani <bruno.patricio@tooseguros.com.br>
2023-11-26 03:43:35 -0300

    first

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculadora {
+
+    public double somar(double valor1, double valor2) {
+        return valor1 + valor2;
+    }
+
+    public double subtrair(double valor1, double valor2) {
+        return valor1 - valor2;
+    }
+
+    public double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+
+    public double divisao(double valor1, double valor2) {
+        return valor1/valor2;
+    }
+
+}
